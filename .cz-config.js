bb8ab868c789ca80f9af4941f861228832974c71
guoxiangwen_cd <guoxiangwen_cd@keruyun.com>
2020-10-26 10:20:32 +0800

    docs(其它): 文档变更

--- .cz-config.js
+++ .cz-config.js
@@ -2,68 +2,24 @@
 
 module.exports = {
   types: [
-    { value: 'feat', name: '✨ feat:     A new feature' },
-    { value: 'fix', name: '🐞 fix:      A bug fix' },
-    { value: 'docs', name: '📚 docs:     Documentation only changes' },
+    { value: 'feat', name: '✨ 功能: 添加新的功能' },
+    { value: 'fix', name: '🐞 修复: 修复一个或多个Bug' },
+    { value: 'docs', name: '📚 文档: 文档变更' },
     {
       value: 'style',
-      name:
-        '🌀 style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+      name: '💅 代码格式: 代码风格更改(空格,分号等)',
     },
     {
       value: 'refactor',
-      name: '🛠 refactor: A code change that neither fixes a bug nor adds a feature',
+      name: '🛠 重构: 重构一个功能,业务模块',
     },
     {
       value: 'perf',
-      name: 'perf:  A code change that improves performance',
+      name: '性能:  提升代码运行效率以及性能',
     },
-    { value: 'test', name: '🏁 test:     Adding missing tests' },
-    {
-      value: 'chore',
-      name: '💅 chore: Changes to the build process or auxiliary tools and libraries such as documentation generation',
-    },
-    { value: 'revert', name: '⏪ revert:   Revert to a commit' },
-    { value: 'wip', name: '💪 wip:      Work in progress' },
+    { value: 'test', name: '🏁 测试: 新加测试用例,或更改已有测试' },
+    { value: 'revert', name: '⏪ 回滚: 代码回滚到某个版本' },
   ],
-  // types: [
-  //   {
-  //     value: 'WIP',
-  //     name: '💪  WIP:      功能未开发完成,待开发中',
-  //   },
-  //   {
-  //     value: '功能',
-  //     name: '✨  功能:     添加新的功能',
-  //   },
-  //   {
-  //     value: '修复',
-  //     name: '🐞  修复:     修复一个或多个Bug',
-  //   },
-  //   {
-  //     value: '重构',
-  //     name: '🛠  重构:      重构一个功能,业务模块',
-  //   },
-  //   {
-  //     value: '文档',
-  //     name: '📚  文档:     文档变更',
-  //   },
-  //   {
-  //     value: '测试',
-  //     name: '🏁   测试:     新加测试用例,或更改已有测试',
-  //   },
-  //   {
-  //     value: '工具',
-  //     name: '🗯  工具:    开发工具变动,构建脚本,脚手架的升级等',
-  //   },
-  //   {
-  //     value: '代码格式',
-  //     name: '💅  代码格式:    代码风格更改(空格,分号等)',
-  //   },
-  //   {
-  //     value: '回滚',
-  //     name: '⏪  回滚:   代码回滚到某个版本',
-  //   },
-  // ],
   messages: {
     type: '选择一种你的提交类型:',
     scope: '选择一个scope (可选):',
@@ -78,6 +34,9 @@ module.exports = {
   scopes: [
     {
       name: '用户模块',
+    },
+    {
+      name: '权限模块',
     },
     {
       name: '其它',
